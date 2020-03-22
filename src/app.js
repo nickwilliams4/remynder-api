29fb9c60d95bda74b654f7586b5733d85df7bffb
nickwilliams4 <50678575+nickwilliams4@users.noreply.github.com>
2020-03-22 12:12:57 -0600

    added db

--- src/app.js
+++ src/app.js
@@ -7,16 +7,19 @@ const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(helmet())
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
+})
+
+app.post('/AddNote', (req, res) => {
+  res.send('Note Added')
 })
 
 app.use(function errorHandler(error, req, res, next) {
