5f5a28b587c107ee9cb4adb193550ffe7b83afa8
nickwilliams4 <50678575+nickwilliams4@users.noreply.github.com>
2020-04-21 11:01:15 -0600

    deleted old tests

--- test/notes-endpoints.spec.js
+++
@@ -1,9 +0,0 @@
-const app = require('../src/app')
-
-describe('notes', () => {
-  it('GET /notes responds with 200 containing "Hello, notes!"', () => {
-    return supertest(app)
-      .get('/notes')
-      .expect(200, 'Hello, notes!')
-  })
-})
--- test/returningusers-endpoints.spec.js
+++
@@ -1,9 +0,0 @@
-const app = require('../src/app')
-
-describe('ReturningUser', () => {
-  it('GET /ReturningUser responds with 200 containing "Hello, ReturningUser!"', () => {
-    return supertest(app)
-      .get('/ReturningUser')
-      .expect(200, 'Hello, ReturningUser!')
-  })
-})
